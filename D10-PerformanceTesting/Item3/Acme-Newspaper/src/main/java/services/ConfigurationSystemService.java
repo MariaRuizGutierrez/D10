53310e2cf067d50dbf11103e84adef23a3fafada
lauverrec <lauraverarec@gmail.com>
2018-04-03 18:40:48 +0200

    Servicio de configuration system

--- D10-PerformanceTesting/Item3/Acme-Newspaper/src/main/java/services/ConfigurationSystemService.java
+++ D10-PerformanceTesting/Item3/Acme-Newspaper/src/main/java/services/ConfigurationSystemService.java
@@ -1,22 +1,73 @@
 
 package services;
 
+import java.util.Collection;
+
 import javax.transaction.Transactional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
+
+import repositories.ConfigurationSystemRepository;
+import domain.ConfigurationSystem;
 
 @Service
 @Transactional
 public class ConfigurationSystemService {
 
 	// Managed repository -----------------------------------------------------
+	@Autowired
+	private ConfigurationSystemRepository	configurationSystemRepository;
 
 	// Supporting services ----------------------------------------------------
 
 	// Constructors -----------------------------------------------------------
+	
+	public ConfigurationSystemService() {
+		super();
+	}
 
 	// Simple CRUD methods ----------------------------------------------------
+	
+	public Collection<ConfigurationSystem> findAll() {
+		Collection<ConfigurationSystem> result;
+
+		Assert.notNull(this.configurationSystemRepository);
+		result = this.configurationSystemRepository.findAll();
+		Assert.notNull(result);
+
+		return result;
+	}
+
+	public ConfigurationSystem findOne(int configurationSystemId) {
+		
+		Assert.isTrue(configurationSystemId != 0);
+		
+		ConfigurationSystem res;
+		
+		res = this.configurationSystemRepository.findOne(configurationSystemId);
+		
+
+		return res;
+
+	}
+
+	public ConfigurationSystem save(final ConfigurationSystem configurationSystem) {
+		Assert.notNull(configurationSystem);
+
+		ConfigurationSystem result;
+
+		result = this.configurationSystemRepository.save(configurationSystem);
+
+		return result;
+	}
+
+
 
 	// Other business methods -------------------------------------------------
+	public void flush() {
+		this.configurationSystemRepository.flush();
+	}
 
 }
