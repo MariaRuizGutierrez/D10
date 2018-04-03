1b288160f2c274c3695ab3f145fd04d85ca68d0d
MariaRuizGutierrez <marikichi1996@gmail.com>
2018-04-03 12:14:13 +0200

    feat: Repositorios y servicios

---
+++ D10-PerformanceTesting/Item3/Acme-Newspaper/src/main/java/services/ChirpService.java
@@ -0,0 +1,22 @@
+
+package services;
+
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+public class ChirpService {
+
+	// Managed repository -----------------------------------------------------
+
+	// Supporting services ----------------------------------------------------
+
+	// Constructors -----------------------------------------------------------
+
+	// Simple CRUD methods ----------------------------------------------------
+
+	// Other business methods -------------------------------------------------
+
+}
