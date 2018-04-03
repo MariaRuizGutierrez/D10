e345a5059346731c076c28507427ff54d81fe34c
espisepi <sepinaco@gmail.com>
2018-04-03 18:20:29 +0200

    feat: findByPrincipal en UserService
    Lo necesitaba para newspaper

--- D10-PerformanceTesting/Item3/Acme-Newspaper/src/main/java/services/UserService.java
+++ D10-PerformanceTesting/Item3/Acme-Newspaper/src/main/java/services/UserService.java
@@ -3,7 +3,14 @@ package services;
 
 import javax.transaction.Transactional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
+
+import repositories.UserRepository;
+import security.LoginService;
+import security.UserAccount;
+import domain.User;
 
 @Service
 @Transactional
@@ -11,11 +18,32 @@ public class UserService {
 
 	// Managed repository -----------------------------------------------------
 
+	@Autowired
+	UserRepository	userRepository;
+
+
 	// Supporting services ----------------------------------------------------
 
 	// Constructors -----------------------------------------------------------
 
+	public UserService() {
+
+	}
+
 	// Simple CRUD methods ----------------------------------------------------
 
 	// Other business methods -------------------------------------------------
+
+	public User findByPrincipal() {
+
+		User result;
+		UserAccount userAccount;
+
+		userAccount = LoginService.getPrincipal();
+		Assert.notNull(userAccount);
+		result = this.userRepository.findByUserAccountId(userAccount.getId());
+		Assert.notNull(result);
+
+		return result;
+	}
 }
