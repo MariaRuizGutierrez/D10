78917d8c8eb15fafad697c654ca31c515ffef01f
joaquinperez94 <blogdeaula78@gmail.com>
2018-04-04 13:51:32 +0200

    Servicios "FollowedUp" y "User"

--- D10-PerformanceTesting/Item3/Acme-Newspaper/src/main/java/services/FollowUpService.java
+++ D10-PerformanceTesting/Item3/Acme-Newspaper/src/main/java/services/FollowUpService.java
@@ -1,21 +1,74 @@
 
 package services;
 
+import java.util.Collection;
+import java.util.Date;
+
 import javax.transaction.Transactional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
+
+import repositories.FollowUpRepository;
+import domain.FollowUp;
 
 @Service
 @Transactional
 public class FollowUpService {
 
 	// Managed repository -----------------------------------------------------
+	@Autowired
+	private FollowUpRepository	followUpRepository;
 
 	// Supporting services ----------------------------------------------------
+	private UserService			userService;
+
 
 	// Constructors -----------------------------------------------------------
+	public FollowUpService() {
+		super();
+	}
 
 	// Simple CRUD methods ----------------------------------------------------
+	public FollowUp create() {
+		Date publicationMoment;
+		FollowUp followUp;
+		//User userConnected;
+
+		publicationMoment = new Date(System.currentTimeMillis() - 1000);
+		//Comprobamos que sea un usuario
+		this.userService.checkPrincipal();
+		//userConnected = this.userService.findByPrincipal();
+		followUp = new FollowUp();
+		followUp.setPublicationMoment(publicationMoment);
+
+		return followUp;
+	}
+
+	public FollowUp save(final FollowUp followUp) {
+		return followUp;
+
+	}
+
+	public void delete(final FollowUp followUp) {
+
+	}
+
+	public FollowUp findOne(final int followUpId) {
+		Assert.isTrue(followUpId != 0);
+		FollowUp result;
+
+		result = this.followUpRepository.findOne(followUpId);
+		return result;
+	}
+
+	public Collection<FollowUp> findAll() {
+		Collection<FollowUp> result;
+
+		result = this.followUpRepository.findAll();
+		return result;
+	}
 
 	// Other business methods -------------------------------------------------
 
--- D10-PerformanceTesting/Item3/Acme-Newspaper/src/main/java/services/UserService.java
+++ D10-PerformanceTesting/Item3/Acme-Newspaper/src/main/java/services/UserService.java
@@ -1,15 +1,21 @@
 
 package services;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.encoding.Md5PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
 
 import repositories.UserRepository;
+import security.Authority;
 import security.LoginService;
 import security.UserAccount;
+import domain.Article;
 import domain.User;
 
 @Service
@@ -27,10 +33,73 @@ public class UserService {
 	// Constructors -----------------------------------------------------------
 
 	public UserService() {
-
+		super();
 	}
 
 	// Simple CRUD methods ----------------------------------------------------
+	public User create() {
+
+		User result;
+		UserAccount userAccount;
+		Authority authority;
+		Collection<Article> articles;
+		Collection<User> followers;
+		Collection<User> followed;
+
+		result = new User();
+		userAccount = new UserAccount();
+		authority = new Authority();
+		articles = new ArrayList<>();
+		followers = new ArrayList<>();
+		followed = new ArrayList<>();
+
+		authority.setAuthority(Authority.USER);
+		userAccount.addAuthority(authority);
+		result.setUserAccount(userAccount);
+		result.setArticles(articles);
+		result.setFollowers(followers);
+		result.setFollowed(followed);
+		return result;
+
+	}
+
+	public User save(final User user) {
+		Assert.notNull(user);
+		User result;
+		Md5PasswordEncoder encoder;
+		String passwordHash;
+
+		if (user.getId() == 0) {
+			encoder = new Md5PasswordEncoder();
+			passwordHash = encoder.encodePassword(user.getUserAccount().getPassword(), null);
+			user.getUserAccount().setPassword(passwordHash);
+		}
+		result = this.userRepository.save(user);
+		Assert.notNull(result);
+		return result;
+	}
+
+	public void delete(final User user) {
+		Assert.notNull(user);
+		Assert.isTrue(user.getId() != 0);
+		this.userRepository.delete(user);
+
+	}
+
+	public Collection<User> findAll() {
+		Collection<User> result;
+
+		result = this.userRepository.findAll();
+		return result;
+	}
+
+	public User findOne(final int userId) {
+		Assert.isTrue(userId != 0);
+		User result;
+		result = this.userRepository.findOne(userId);
+
+		return result;
+	}
 
 	// Other business methods -------------------------------------------------
 
@@ -45,5 +114,20 @@ public class UserService {
 		Assert.notNull(result);
 
 		return result;
+	}
+
+	public void checkPrincipal() {
+
+		final UserAccount userAccount = LoginService.getPrincipal();
+
+		Assert.notNull(userAccount);
+
+		final Collection<Authority> authorities = userAccount.getAuthorities();
+		Assert.notNull(authorities);
+
+		final Authority auth = new Authority();
+		auth.setAuthority("RANGER");
+
+		Assert.isTrue(authorities.contains(auth));
 	}
 }
