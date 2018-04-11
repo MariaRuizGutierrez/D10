99b917b941ef39aa71632013f10e135215ebe491
MariaRuizGutierrez <marikichi1996@gmail.com>
2018-04-11 20:42:40 +0200

    Cambio en un test y populate bien

--- D10-PerformanceTesting/Item3/Acme-Newspaper/src/test/java/services/AdminServiceTest.java
+++ D10-PerformanceTesting/Item3/Acme-Newspaper/src/test/java/services/AdminServiceTest.java
@@ -30,19 +30,19 @@ public class AdminServiceTest extends AbstractTest {
 	// Supporting services ----------------------------------------------------
 
 	@Autowired
-	private AdminService	adminService;
-	
+	private AdminService		adminService;
+
 	@Autowired
-	private ArticleService 	articleService;
-	
+	private ArticleService		articleService;
+
 	@Autowired
-	private NewspaperService newspaperService;
-	
+	private NewspaperService	newspaperService;
+
 	@Autowired
-	private ChirpService chirpService;
+	private ChirpService		chirpService;
 
 	@PersistenceContext
-	EntityManager			entityManager;
+	EntityManager				entityManager;
 
 
 	//Login 
@@ -149,7 +149,7 @@ public class AdminServiceTest extends AbstractTest {
 		this.checkExceptions(expected, caught);
 
 	}
-	
+
 	//Use case 7.1. An admin removes an article that he or she thinks is inappropriate.
 	@Test
 	public void driveRemoveArticleAdmin() {
@@ -159,7 +159,7 @@ public class AdminServiceTest extends AbstractTest {
 			{
 				"admin", "article2", null
 			},
-			
+
 			//Admin can't remove article 1 because his newspaper have subscriptions. Negative case
 			{
 				"admin", "article1", java.lang.IllegalArgumentException.class
@@ -172,15 +172,13 @@ public class AdminServiceTest extends AbstractTest {
 			{
 				"customer1", "article2", java.lang.IllegalArgumentException.class
 			}
-			
+
 		};
 
 		for (int i = 0; i < testingData.length; i++)
-			this.templateRemoveArticleAdmin((String) testingData[i][0], (String) testingData[i][1], 
-				(Class<?>) testingData[i][2]);
+			this.templateRemoveArticleAdmin((String) testingData[i][0], (String) testingData[i][1], (Class<?>) testingData[i][2]);
 
 	}
-	
 
 	public void templateRemoveArticleAdmin(final String username, final String articleEntity, final Class<?> expected) {
 
@@ -189,7 +187,6 @@ public class AdminServiceTest extends AbstractTest {
 
 		caught = null;
 		article = this.articleService.findOne(super.getEntityId(articleEntity));
-		
 
 		try {
 			super.authenticate(username);
@@ -204,7 +201,7 @@ public class AdminServiceTest extends AbstractTest {
 		this.checkExceptions(expected, caught);
 
 	}
-	
+
 	//Use case 7.2. An admin removes a newspaper that he or she thinks is inappropriate.
 	@Test
 	public void driveRemoveNewspaperAdmin() {
@@ -214,7 +211,7 @@ public class AdminServiceTest extends AbstractTest {
 			{
 				"admin", "newspaper2", null
 			},
-			
+
 			//Admin can't remove newspaper 1 because it have subscriptions. Negative case
 			{
 				"admin", "newspaper1", java.lang.IllegalArgumentException.class
@@ -227,15 +224,13 @@ public class AdminServiceTest extends AbstractTest {
 			{
 				"customer1", "newspaper3", java.lang.IllegalArgumentException.class
 			}
-			
+
 		};
 
 		for (int i = 0; i < testingData.length; i++)
-			this.templateRemoveNewspaperAdmin((String) testingData[i][0], (String) testingData[i][1], 
-				(Class<?>) testingData[i][2]);
+			this.templateRemoveNewspaperAdmin((String) testingData[i][0], (String) testingData[i][1], (Class<?>) testingData[i][2]);
 
 	}
-	
 
 	public void templateRemoveNewspaperAdmin(final String username, final String newspaperEntity, final Class<?> expected) {
 
@@ -244,7 +239,6 @@ public class AdminServiceTest extends AbstractTest {
 
 		caught = null;
 		newspaper = this.newspaperService.findOne(super.getEntityId(newspaperEntity));
-		
 
 		try {
 			super.authenticate(username);
@@ -259,7 +253,7 @@ public class AdminServiceTest extends AbstractTest {
 		this.checkExceptions(expected, caught);
 
 	}
-	
+
 	//Use case 7.2. An admin removes a chirp that he or she thinks is inappropriate.
 	@Test
 	public void driveRemoveChirpAdmin() {
@@ -269,7 +263,7 @@ public class AdminServiceTest extends AbstractTest {
 			{
 				"admin", "chirp2", null
 			},
-			
+
 			//User can't remove a chirp. Negative case
 			{
 				"user1", "chirp1", java.lang.IllegalArgumentException.class
@@ -278,15 +272,13 @@ public class AdminServiceTest extends AbstractTest {
 			{
 				"customer1", "chirp1", java.lang.IllegalArgumentException.class
 			}
-			
+
 		};
 
 		for (int i = 0; i < testingData.length; i++)
-			this.templateRemoveChirpAdmin((String) testingData[i][0], (String) testingData[i][1], 
-				(Class<?>) testingData[i][2]);
+			this.templateRemoveChirpAdmin((String) testingData[i][0], (String) testingData[i][1], (Class<?>) testingData[i][2]);
 
 	}
-	
 
 	public void templateRemoveChirpAdmin(final String username, final String chirpEntity, final Class<?> expected) {
 
@@ -295,7 +287,6 @@ public class AdminServiceTest extends AbstractTest {
 
 		caught = null;
 		chirp = this.chirpService.findOne(super.getEntityId(chirpEntity));
-		
 
 		try {
 			super.authenticate(username);
@@ -310,7 +301,7 @@ public class AdminServiceTest extends AbstractTest {
 		this.checkExceptions(expected, caught);
 
 	}
-	
+
 	//Use case 17.2. An admin lists the articles that contains taboo words
 	@Test
 	public void driveListArticleWithTabooWordAdmin() {
@@ -320,7 +311,7 @@ public class AdminServiceTest extends AbstractTest {
 			{
 				"admin", "article2", null
 			},
-			
+
 			//User can't list articles. Negative case
 			{
 				"user1", "article2", java.lang.IllegalArgumentException.class
@@ -329,25 +320,22 @@ public class AdminServiceTest extends AbstractTest {
 			{
 				"customer1", "article2", java.lang.IllegalArgumentException.class
 			}
-			
+
 		};
 
 		for (int i = 0; i < testingData.length; i++)
-			this.templateListArticleWithTabooWordAdmin((String) testingData[i][0], (String) testingData[i][1], 
-				(Class<?>) testingData[i][2]);
+			this.templateListArticleWithTabooWordAdmin((String) testingData[i][0], (String) testingData[i][1], (Class<?>) testingData[i][2]);
 
 	}
-	
 
 	public void templateListArticleWithTabooWordAdmin(final String username, final String articleEntity, final Class<?> expected) {
 
 		Class<?> caught;
 		Article article;
 		Collection<Article> articles;
-		
+
 		caught = null;
 		article = this.articleService.findOne(super.getEntityId(articleEntity));
-		
 
 		try {
 			super.authenticate(username);
@@ -363,7 +351,7 @@ public class AdminServiceTest extends AbstractTest {
 		this.checkExceptions(expected, caught);
 
 	}
-	
+
 	//Use case 17.3. An admin lists the newspapers that contains taboo words
 	@Test
 	public void driveListNewspaperWithTabooWordAdmin() {
@@ -371,9 +359,9 @@ public class AdminServiceTest extends AbstractTest {
 		final Object testingData[][] = {
 			//Admin list newspapers, positive case
 			{
-				"admin", "newspaper1", null
+				"admin", "newspaper2", null
 			},
-			
+
 			//User can't list newspapers. Negative case
 			{
 				"user1", "newspaper1", java.lang.IllegalArgumentException.class
@@ -382,25 +370,22 @@ public class AdminServiceTest extends AbstractTest {
 			{
 				"customer1", "newspaper1", java.lang.IllegalArgumentException.class
 			}
-			
+
 		};
 
 		for (int i = 0; i < testingData.length; i++)
-			this.templateListNewspaperWithTabooWordAdmin((String) testingData[i][0], (String) testingData[i][1], 
-				(Class<?>) testingData[i][2]);
+			this.templateListNewspaperWithTabooWordAdmin((String) testingData[i][0], (String) testingData[i][1], (Class<?>) testingData[i][2]);
 
 	}
-	
 
 	public void templateListNewspaperWithTabooWordAdmin(final String username, final String newspaperEntity, final Class<?> expected) {
 
 		Class<?> caught;
 		Newspaper newspaper;
 		Collection<Newspaper> newspapers;
-		
+
 		caught = null;
 		newspaper = this.newspaperService.findOne(super.getEntityId(newspaperEntity));
-		
 
 		try {
 			super.authenticate(username);
@@ -416,7 +401,7 @@ public class AdminServiceTest extends AbstractTest {
 		this.checkExceptions(expected, caught);
 
 	}
-	
+
 	//Use case 17.3. An admin lists the chirps that contains taboo words
 	@Test
 	public void driveListChirpsWithTabooWordAdmin() {
@@ -426,7 +411,7 @@ public class AdminServiceTest extends AbstractTest {
 			{
 				"admin", "chirp1", null
 			},
-			
+
 			//User can't list newspapers. Negative case
 			{
 				"user1", "chirp1", java.lang.IllegalArgumentException.class
@@ -435,25 +420,22 @@ public class AdminServiceTest extends AbstractTest {
 			{
 				"customer1", "chirp1", java.lang.IllegalArgumentException.class
 			}
-			
+
 		};
 
 		for (int i = 0; i < testingData.length; i++)
-			this.templateListChirpsWithTabooWordAdmin((String) testingData[i][0], (String) testingData[i][1], 
-				(Class<?>) testingData[i][2]);
+			this.templateListChirpsWithTabooWordAdmin((String) testingData[i][0], (String) testingData[i][1], (Class<?>) testingData[i][2]);
 
 	}
-	
 
 	public void templateListChirpsWithTabooWordAdmin(final String username, final String newspaperEntity, final Class<?> expected) {
 
 		Class<?> caught;
 		Chirp chirp;
 		Collection<Chirp> chirps;
-		
+
 		caught = null;
 		chirp = this.chirpService.findOne(super.getEntityId(newspaperEntity));
-		
 
 		try {
 			super.authenticate(username);
@@ -469,7 +451,5 @@ public class AdminServiceTest extends AbstractTest {
 		this.checkExceptions(expected, caught);
 
 	}
-	
-		
 
 }
